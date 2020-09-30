5a9fecddf382fef3751b96d538bd141d5ecd322c
toufik-airane <toufik.airane@gmail.com>
2020-09-30 19:38:59 +0200

    update

--- main.go
+++ main.go
@@ -56,6 +56,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("%s\n", config)
 
 	for index := range config.Checks {
 		config.Checks[index].Compiled = regexp.MustCompile(config.Checks[index].Regex)
